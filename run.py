f3df590d5abc2eda8d7e20d6ea6418c36281929c
Sophie Thomson <sophielbenoit@gmail.com>
2024-08-07 16:17:52 +0000

    Add json data and styling to About Page.

--- run.py
+++ run.py
@@ -1,24 +1,35 @@
 import os
+import json
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 #  Decorator starts with @ to wrap functions
 #  app.route() function binds a file to the 'view' of the file with that name
 #  Flask command 'render_template' looks in 'templates' folder for the file passed to the function
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Home")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
